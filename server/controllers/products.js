559a1792eb29dc456f879216c54d91df57020d0f
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-08-26 23:15:42 -0400

    feat(server): realize GET querys in getAllProducts controller with params featured company and name

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,10 +1,29 @@
+import Products from '../models/products.js';
 
 export const getAllProductsStatic = async (req, resp) => {
-  throw new Error('testing async errors');
-  resp.status(200).json({msg: 'test getAllProductsStatic route'});
+  const search = 'ab';
+  const products = await Products.find({
+    name: { $regex: search, $options: 'i' },
+  });
+  resp.status(200).json({ products, nbHits: products.length });
 };
 
 export const getAllProducts = async (req, resp) => {
-  resp.status(200).json({msg: 'test getAllProducts route'});
+  const { featured, company, name } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: 'i' };
+  }
+
+  console.log(queryObj)
+  const products = await Products.find(queryObj);
+  resp.status(200).json({ products, nbHits: products.length });
 };
 
--- server/models/products.js
+++ server/models/products.js
@@ -1,37 +1,33 @@
 import mongoose from 'mongoose';
 
-const ProductsSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Products nane must be provided'],
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name must be provided'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price must be provided'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
+      message: '{VALUE} is not supported',
+    },
+  },
+})
 
-    },
-    price: {
-      type: Number,
-      required: [true, 'Products price must be provided'],
-    },
-    featured: {
-      type: Boolean,
-      default: false,
-    },
-    rating: {
-      type: Number,
-      default: 4.5,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    company: {
-      type: String,
-      enum: {
-        values: ['ikea', 'liddy', 'caressa', 'marcos'],
-        message: '{VALUE} is not supported',
-      },
-      // enum: ['ikea', 'liddy', 'caressa', 'marcos'],
-    },
-  }
-);
-
-export default mongoose.model('Products', ProductsSchema);
+export default mongoose.model('Products', productSchema);

a5edcfb282b1c376c8e62c9f70e5ecc28cbb9aa3
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-08-27 20:41:17 -0400

    feat(server): add search query params, in getAllProducts methods

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,19 +1,16 @@
 import Products from '../models/products.js';
 
 export const getAllProductsStatic = async (req, resp) => {
-  const search = 'ab';
-  const products = await Products.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Products.find({}).sort('-name price');
   resp.status(200).json({ products, nbHits: products.length });
 };
 
 export const getAllProducts = async (req, resp) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObj = {};
 
   if (featured) {
-    queryObj.featured = featured === 'true' ? true : false;
+    queryObj.featured = (featured === 'true');
   }
   if (company) {
     queryObj.company = company;
@@ -23,7 +20,14 @@ export const getAllProducts = async (req, resp) => {
   }
 
   console.log(queryObj)
-  const products = await Products.find(queryObj);
+  let result = Products.find(queryObj);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result.sort('createdAt');
+  }
+  const products = await result;
   resp.status(200).json({ products, nbHits: products.length });
 };
 
